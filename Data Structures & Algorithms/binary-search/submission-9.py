6b32bd22e7d07f143eab772f41c055136cb96f04
Ilyasahallal <159455114+Ilyasahallal@users.noreply.github.com>
2026-09-19 12:58:17 +0100

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        a=0
+        b=n-1
+        while a<=b:
+            m=(a+b)//2
+            if target < nums[m] :
+                b=m-1
+            elif target > nums[m] :
+                a=m+1
+            else :
+                return m
+        return -1
+
